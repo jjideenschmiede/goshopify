e151d527a12e9165dda1f51064337f6358da3534
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-07-28 11:34:04 +0200

    Add config file

--- config.go
+++ config.go
@@ -10,3 +10,54 @@
 //**********************************************************
 
 package goshopify
+
+import (
+	"bytes"
+	"net/http"
+)
+
+const (
+	transferProtocol = "https://"
+	baseUrl          = ".myshopify.com/admin/api/"
+	apiVersion       = "2021-07"
+)
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	Body         []byte
+}
+
+// Request is to define the request data
+type Request struct {
+	ApiKey, ApiPassword, StoreName string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r *Request) (*http.Response, error) {
+
+	// Set url
+	url := transferProtocol + r.ApiKey + ":" + r.ApiPassword + "@" + r.StoreName + baseUrl + apiVersion + c.Path
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Define header
+	request.Header.Set("Content-Type", "application/json")
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
