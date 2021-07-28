9f9d04bb4cbf17ec45d12045abde0b1c482fe06f
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-07-28 11:33:21 +0200

    Initial commit

---
+++ config.go
@@ -0,0 +1,12 @@
+//**********************************************************
+//
+// Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of goshopify.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor
+//
+//**********************************************************
+
+package goshopify
